61742167b5d649167c6acde8196a2a155e8b5d88
Krzysztof Jachura <81533443+krzysztof-jachura@users.noreply.github.com>
2024-12-15 13:45:13 +0100

    chore: Added Vite React SWC plugin and saved npm audit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
     'process.env': process.env,
   },
   base: process.env.BASEPATH,
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     host: '0.0.0.0',
     port: 5173,
